2fb98754644e48d0692627060d13984b678a98e4
“Denys <denn8051@gmail.com>
2025-09-13 19:04:19 +0200

    add different .env for development and production

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,17 @@ import { AppService } from "./app.service";
 import { UserModule } from "./user/users.module";
 import { AuthModule } from "./auth/auth.module";
 import { AppController } from "./app.controller";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UserModule, AuthModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+  ],
 })
 export class AppModule {}
